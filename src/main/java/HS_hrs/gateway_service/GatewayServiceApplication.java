1d053b213507e5137f977ea07145560ea99ec267
hyunwoo <kshsim98@naver.com>
2025-04-15 11:25:22 +0900

    gateway

---
+++ src/main/java/HS_hrs/gateway_service/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package HS_hrs.gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HS_hrs/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package HS_hrs.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
